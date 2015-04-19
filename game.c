94b7e5b571abeb0077c74f5e57d8065b67859f55
Alexander Lin <alexanderlin@college.harvard.edu>
2015-04-18 21:20:36 -0400

    testing for alexander

--- game.c
+++ game.c
@@ -3,6 +3,6 @@
 #include <stdio.h>
 
 int main() {
-  po_vector my_vector = {.x=0.0, .y=1.0};
+  po_vector my_vector = {.x=0.0,.y=1.0};
   printf("my_vector: <%f, %f>\n", my_vector.x, my_vector.y);
 }
