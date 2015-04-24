5d1dec13530c4378fef70ea98cdf7826e54679de
Annaleah Ernst <ANNALEAHERNST@COLLEGE.HARVARD.EDU>
2015-04-24 00:50:30 -0400

    yay pointers fixed!

--- physics.c
+++ physics.c
@@ -8,7 +8,7 @@
 #include "physics.h"
 
 /* the actual implementation of a physics object structure */
-struct po_imp {
+typedef struct po_imp {
   // change in x, y, and rotations
   float dx;
   float dy;
@@ -25,99 +25,118 @@ struct po_imp {
 }po_imp;
 
 /* for our silly linked list version of world 
- * this will not be a thing later, hopefuly */
+ * this will not be a thing later, hopefully
 /* actual implementation of physics world structure */
-struct world_t {
-  /*struct po_handle object;
-    struct world_t* next;*/
-  int placeholder;
+typedef struct world_t {
+  po_handle object;
+  struct world_t* next;
 } world_t;
 
-typedef world_t* world_handle;
-typedef po_imp* po_handle;
-
 /* create a new world */
-world_handle new_world(){
-
-  return NULL;
+/* basically, just an empty piece of memory */
+world_handle new_world ()
+{
+  world_t new_first;
+  new_first.object = NULL;
+  new_first.next = NULL;
+  return &new_first;
 }
 
 /* add object to the physics world */
-/*
 po_handle add_object (world_handle world, po_geometry* geom, 
 		      float x, float y, float r)
 {
   // make a new object
-  struct po_handle new_obj;
-  new_obj.x = x;
-  new_obj.y = y;
-  new_obj.r = r;
-  new_obj.dx = 0;
-  new_obj.dy = 0;
-  new_obj.dr = 0;
-  new_obj.object = geom; 
-  
-  struct world_handle new_first;
-  new_first.object = new_obj;
-  new_first.next = world;
-  return new_world;
+  po_handle new_obj;
+  new_obj->x = x;
+  new_obj->y = y;
+  new_obj->r = r;
+  new_obj->dx = 0;
+  new_obj->dy = 0;
+  new_obj->dr = 0;
+  new_obj->object = *geom; 
 }
-*/
 
 /* Updates object's global position based on velocity
  * Future versions may include more sophistocated algorthims using acceleration */
 void integrate (float dx, float dy, float dr, float time_step, po_handle obj) {
+  // check input
+  if (obj == NULL) { 
+    return 1;
+  }
+
   // apply euler's method
-  
-  obj.x = obj.x + (dx * time_step);
-  obj.y = obj.y + (dy * time_step);
-  obj.r = obj.r + (dr * time_step);
+  obj->x = obj->x + (dx * time_step);
+  obj->y = obj->y + (dy * time_step);
+  obj->r = obj->r + (dr * time_step);
+  return 0;
 }
 
 int set_location (float x, float y, po_handle obj) {
-  /* // check input
+  // check input
   if (obj == NULL) { 
     return 1;
-  } */
+  }
 
   // update location, return success
-  *obj.x = x;
-  *obj.y = y;
+  obj->x = x;
+  obj->y = y;
   return 0;
 }
 
 int set_rotation (float r, po_handle obj) {
-  /*  // check input
+  // check input
   if (obj == NULL) {
     return 1;
-    } */
+    }
 
   // update rotation, return success
-  obj.r = r;
+  obj->r = r;
   return 0;
 }
 
 int set_velocity (float dx, float dy, po_handle obj) {
-  /*  // check input
+  // check input
   if (obj == NULL) {
     return 1;
-    } */
+    }
 
   // update velocity, return success
-  obj.dx = dx;
-  obj.dy = dy;
+  obj->dx = dx;
+  obj->dy = dy;
   return 0;
 }
 
 int set_angular_vel (float dr, po_handle obj) {
-  /*  // check input
+  // check input
   if (obj == NULL) {
     return 1;
-    } */
+    }
 
   // update rotation, return success
-  obj.dr = dr;
+  obj->dr = dr;
   return 0;
 
 }
 
+void detect_collision();
+// check collision for every clock tick
+// really, check if things are collided or would have been collided
+
+
+void coll_broadphase (world_handle world);
+// create new, smaller world -> a bucket if you will
+// (what exactly this is depends on world implementation)
+// only possible things that will collide
+// if no object in bucket, or one object in bucket, skip
+// call midphase on each of the small worlds we've created
+
+void coll_midphase();
+// takes the objects in the hash buckets passed by broadphase
+// draws bounding boxes around these objects
+// detects overlap between bounding boxes
+// if overlap, call narrowphase
+
+void coll_narrowphase();
+// parallel axis theorem on objects that might collide
+// if collision, call resolve collsion (with two objects)? set collision flag?
