c072f1bcd3dba70661720e9bf621a6706d4bd74e
Thomas Lively <tlively@college.harvard.edu>
2015-04-28 01:43:05 -0400

    it compiles at least

--- Makefile
+++ Makefile
@@ -3,17 +3,20 @@
 all: game
 	@echo "building all!"
 
-game: game.c physics.o graphics.o
+game: game.c physics.o graphics.o dynamic_array.o
 	gcc -Werror -o game game.c physics.o graphics.o `sdl2-config --cflags --libs` -lm -std=c99
 
 test: physics.o graphics.o
 	gcc -Werror -o test test.c physics.o graphics.o `sdl2-config --cflags --libs` -lm -std=c99 && ./test
 
-physics.o: physics.c
+physics.o: physics.c dynamic_array.o
 	gcc -Werror -o physics.o -c physics.c -lm -std=c99
 
 graphics.o: graphics.c
 	gcc -Werror -o graphics.o -c graphics.c `sdl2-config --cflags --libs` -lm -std=c99
+
+dynamic_array.o: dynamic_array.c
+	gcc -Werror -o dynamic_array.o -c dynamic_array.c `sdl2-config --cflags --libs` -lm -std=c99
 
 clean:
 	rm -f game test physics.o graphics.o
---
+++ dynamic_array.h
@@ -0,0 +1,28 @@
+/***************************************************************************
+ * dynamic_array.h - solving all of your dynamic indexing needs since 2015 *
+ ***************************************************************************/
+#ifdef DYNAMIC_ARRAY_INCL
+
+struct dynamic_array;
+typedef struct dynamic_array dynamic_array;
+
+// set up a newly created dynamic array with all of the appropriate internals.
+void dynamic_array_init(dynamic_array* da);
+
+// add the data pointer to the array at index "index". Indices can be negative.
+void dynamic_array_add(dynamic_array* da, int index, void* data);
+
+// returns the item at the given index or NULL if it doesn't exist.
+void* dynamic_array_get(dynamic_array* da, int index);
+
+// same as dynamic_array_get but also removes the item as a side effect.
+void* dynamic_array_remove(dynamic_array* da, int index);
+
+// reduce total memory usage by removing padding on the edges of da.
+void dynamic_array_shrink(dynamic_array* da);
+
+// safely and cleanly frees all memory associated with da.
+void dynamic_array_free(dynamic_array* da);
+
+#endif
+#define DYNAMIC_ARRAY_INCL
