36d6d3c2e6ee2ce161b6ede2c270a7f3bba46cd7
Annaleah Ernst <ANNALEAHERNST@COLLEGE.HARVARD.EDU>
2015-04-29 00:34:50 -0400

    header for collisions so they can appear in a rational order

---
+++ collisions.h
@@ -0,0 +1,28 @@
+/***********************************************************
+ * Collisions.c
+ * header for functions associated with colliding things into things
+ ***********************************************************/
+
+/* broadphase of collision detection: spatial hashing */
+void coll_broadphase (world_handle world);
+
+/* currently nonfunctional */
+/* in the future, will be bounding boxes */
+void coll_midphase(po_handle bucket1, po_handle bucket2);
+
+/* currently for circles only */
+/* future iterations will be parallel axis theorem */
+void coll_narrowphase(po_handle obj1, po_handle obj2);
+
+/* helper function for broadphase collision detection 
+ * takes in a single row array and checks adjacent cells for contents */
+void check_row(dynamic_array* row_k, int k_min, int k_max);
+
+/* helper function for broadphase collision detection
+ * takes in current row and next row and checks each cell in the top row 
+ * for contents and contents in the bottom two buckets */
+void check_rows(dynamic_array* row_k, int k_min, int k_max, 
+		dynamic_array* row_kplus);
+
+
+
