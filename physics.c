9ecf1476963584a1abdcb4eb4864cd5b20aca6df
Annaleah Ernst <ANNALEAHERNST@COLLEGE.HARVARD.EDU>
2015-05-01 02:48:47 -0400

    in the procress of fixing

--- physics.c
+++ physics.c
@@ -490,7 +490,7 @@ void coll_narrowphase(po_handle obj1, po_handle obj2) {
   // check the object types and act accordingly
   if (!SHAPE_TYPE(obj1) && !SHAPE_TYPE(obj2)) {  
     // sum of radii squared
-    float r_2 = pow(obj1->shape.radius,2.0) + pow(obj2->shape.radius,2.0);
+    float r_2 = pow(CIRC(obj1).radius,2.0) + pow(CIRC(obj2).radius,2.0);
 
     // there's a collision, resolve it
     if(distance_squared(obj1->centroid,obj2->centroid) <= r_2){
@@ -578,8 +578,10 @@ void get_normals (po_vector* verts, int size, po_vector** normals) {
 
 /* go through the sides of poly1 comparing with the verts of poly2 
  * to get the vertex that is poking through 
+ * returns 1 on failure, 0 on success
  * if we don't find anything, we need to switch inputs and try again */
-po_vector find_intersection (po_handle po_pts, po_handle po_sides){
+int find_intersection (po_handle po_pts, po_handle po_sides, 
+			     int* index_pt, int* index_sides){
   // the polygon we're doing corner stuff with 
   po_vector* vert_pts;
   get_global_coord(po_pts, &vert_pts);
@@ -591,27 +593,60 @@ po_vector find_intersection (po_handle po_pts, po_handle po_sides){
   get_normals(vert_sides, NVERTS(po_sides), &normals);
 
   // the outer loops is for the points in the first poly
-  for (int i = 0; i < NVERTS(po_pts); i++){
-    // go through the other vertices
+  for (int i = 0, max_j = NVERTS(po_sides); i < NVERTS(po_pts); i++){
+    // these will keep track of our smallest magnitude dot prods; resets every new vert
+    int min_dot_prod = 0;
+    *index_sides = 0;
+
+    // go through the vertices of po_pts
     for (int j = 0; j < NVERTS(po_sides); j++) {      
-      // get the vector from the 
+      // get the vector from a corner to 
       po_vector corner_to_point = vect_from_points(vert_pts[i], vert_sides[j]);
-      if (0 > vect_dot_prod(normals[j], corner_to_point)){
-	// no intersection, skip the rest of this point
-        continue;
+      float cur_dot_prod = vect_dot_prod(normals[j], corner_to_point);
+      if (0 > cur_dot_prod){
+        // no intersection, skip the rest of the dot prods
+        break;
+      }
+    
+      if (-cur_dot_prod > min_dot_prod){
+	// update our maxes
+	*index_sides = j;
+	min_dot_prod = -cur_dot_prod;
+      }
+
+      if (j == max_j) {
+	// we've made it through the whole loop without sadness
+	*index_pt = i;
+	return 0;
       }
     } 
-    // there is one if we make it through the for loop w/o a continue
   }
+  // we didn't get anything so we need to tell the calling function
+  return 1;
 }
 
 // TODO: make this a thing, takes two polys and resolves coll
-int resolve_coll_polys (po_handle poly1, po_handle poly2){
+void resolve_coll_polys (po_handle poly1, po_handle poly2) {
   // determine which point and which side had a collision
+  int index1, index2;
 
+  // lets us know which shape is the intersector, which the intersectee
+  int which_shape = 0;
+  if (find_intersection(poly1, poly2, &index1,&index2)){
+    // then we have the polygon order wrong
+    if (poly2, poly1, &index_vert, &index_vect){
+      // then there's not a collision. Do we handle or just return or...?
+      return;
+    }
+    // the index1 is associate with poly2, the index_vect is associated with poly1
+    int temp = index1;
+    index1 = index2;
+    index2 = temp;
+    which_shape = 1;
+  }
 }
 
-//TODO:make this a thing: takes a poly and a circ and resolves
+//TODO: make this a thing: takes a poly and a circ and resolves
 int resolve_coll_mixed (po_handle poly, po_handle circ){} 
 
 /***************************************************************
